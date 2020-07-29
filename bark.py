72e1028bd7efda22f33206f1884c89e75b978507
EdgProgrammer <edg.programmer@gmail.com>
2020-07-29 18:02:30 -0400

    Fixed problems

--- bark.py
+++ bark.py
@@ -1,32 +1,60 @@
-import serial, json
+import sys
 import time
 import os
 
-s = serial.Serial('/dev/cu.usbmodem14101')
-status = 0
+if 'serial' not in sys.modules:
+    os.system("python -m pip install pyserial")
+if 'playsound' not in sys.modules:
+    os.system("python -m pip install playsound")
 
-def toggleFunction():
-    global status
+from serial import Serial
+from  playsound import playsound
+import json
 
-    if status == 0:
-        os.system("open -a Terminal")
-        status = 1
-    else:
-        os.system("open -a Terminal")
-        status = 1
+open_ports = os.popen("ls /dev/cu.*").read().split("\n")
+kano_port = ""
 
-while True:
-    try:
-        data = json.loads(s.readline())
-        proximity = (data["detail"]["proximity"])
+for port in open_ports:
+    if "cu.usbmodem" in port:
+        kano_port = port
+        break
 
-        print(proximity)
+if kano_port == "":
+    print("Please plug in KANO Motion Sensor to continue")
+else:
+    s = Serial(kano_port)
+    previous_proximity = 0
+    status = 0
 
-        if proximity > 0:
-            toggleFunction()
-            print("\n\n\n\n" + lights_status + "\n\n\n\n")
-            time.sleep(3)
+    def toggleFunction():
+        global status
 
-        s.flushInput()
-    except Exception:
-        continue
+        if status == 0:
+            playsound("dog_bark.mp3")
+            print("playing sound")
+            status = 1
+        else:
+            playsound("cat_meow.mp3")
+            print("playing sound")
+            status = 0
+
+    while True:
+        try:
+            data = json.loads(s.readline())
+            proximity = (data["detail"]["proximity"])
+
+            if previous_proximity > 0:
+                proximity = 0
+
+            if proximity > 0:
+                print("Motion Detected")
+                toggleFunction()
+                time.sleep(0.2)
+            else:
+                print("No Motion")
+
+            previous_proximity = proximity
+
+            s.flushInput()
+        except Exception:
+            continue
